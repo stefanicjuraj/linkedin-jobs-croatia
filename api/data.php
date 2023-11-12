7188f46f64e5673c4bcf4ce9b9876e6d8df360dc
stefanicjuraj <jurajstefanic@outlook.com>
2023-11-12 20:46:59 +0100

    feat: add data scraper script

--- api/data.php
+++ api/data.php
@@ -26,7 +26,51 @@
 
     <?php include 'sidebar.php' ?>
 
+    <div class="mt-10 p-4">
+
+        <script type="text/javascript">
+            rssfeed_url = new Array();
+            rssfeed_url[0] = "https://politepol.com/fd/hy5fKt0iNR1f";
+            rssfeed_frame_width = "550";
+            rssfeed_frame_height = "1000";
+            rssfeed_scroll = "off";
+            rssfeed_scroll_step = "4";
+            rssfeed_scroll_bar = "on";
+            rssfeed_target = "_blank";
+            rssfeed_font_size = "15";
+            rssfeed_font_face = "Arial, sans-serif";
+            rssfeed_border = "off";
+            rssfeed_css_url = "https://linkedin-scraper-css.vercel.app/rss.css";
+            rssfeed_title = "off";
+            rssfeed_title_name = "";
+            rssfeed_title_bgcolor = "#333";
+            rssfeed_title_color = "#fff";
+            rssfeed_title_bgimage = "";
+            rssfeed_footer = "off";
+            rssfeed_footer_name = "";
+            rssfeed_footer_bgcolor = "#fff";
+            rssfeed_footer_color = "#333";
+            rssfeed_footer_bgimage = "";
+            rssfeed_item_title_length = "100";
+            rssfeed_item_title_color = "#666";
+            rssfeed_item_bgcolor = "#fff";
+            rssfeed_item_bgimage = "";
+            rssfeed_item_border_bottom = "on";
+            rssfeed_item_source_icon = "on";
+            rssfeed_item_date = "off";
+            rssfeed_item_description = "on";
+            rssfeed_item_description_length = "250";
+            rssfeed_item_description_color = "#666";
+            rssfeed_item_description_link_color = "#333";
+            rssfeed_item_description_tag = "on";
+            rssfeed_no_items = "0";
+            rssfeed_cache = "20aca96eb8690e0470962bf2d33cf95c";
+        </script>
+
+    </div>
+
     <!-- script -->
+    <script type="text/javascript" src="https://linkedin-scraper-css.vercel.app/app.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.js"></script>
 
 </body>
