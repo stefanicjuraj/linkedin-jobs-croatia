c60241ed9cd1d2cdb2ca227fd8e7e6663fdc7747
stefanicjuraj <jurajstefanic@outlook.com>
2023-11-12 20:31:30 +0100

    feat: add html skeleton

--- api/index.php
+++ api/index.php
@@ -1,2 +1,22 @@
 <?php
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>linkedin-scraper</title>
+    <!-- css -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+
+    <!-- script -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.js"></script>
+
+</body>
+
+</html>
