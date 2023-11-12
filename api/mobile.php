68ebec236e7065f2ed07e6b8907f5645d05d9b0d
stefanicjuraj <jurajstefanic@outlook.com>
2023-11-12 20:40:59 +0100

    feat: add html to mobile

--- api/mobile.php
+++ api/mobile.php
@@ -1,2 +1,32 @@
 <?php
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>linkedin-scraper</title>
+    <!-- css -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<style>
+    iframe {
+        margin-left: auto;
+        margin-right: auto;
+        text-align: center;
+        background-color: #fff;
+        max-width: 100%;
+    }
+</style>
+
+<body>
+
+    <!-- script -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.js"></script>
+
+</body>
+
+</html>
