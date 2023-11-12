abd9a14ae10e05ba95d635976be93b517e71675b
stefanicjuraj <jurajstefanic@outlook.com>
2023-11-12 20:48:57 +0100

    feat: add include sidebar to index

--- api/index.php
+++ api/index.php
@@ -39,6 +39,8 @@ if ($_SERVER['REQUEST_URI'] === '/frontend') {
 
 <body>
 
+    <?php include 'sidebar.php' ?>
+
     <!-- script -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.js"></script>
 
