f3f9b4acf7acf918b79fb9ed6bbb0c64e16fc6a8
stefanicjuraj <jurajstefanic@outlook.com>
2023-11-23 12:05:03 +0100

    feat: add typography & font family to body

--- api/departments/project-management.php
+++ api/departments/project-management.php
@@ -10,9 +10,17 @@
     <title>linkedin-scraper</title>
     <!-- css -->
     <script src="https://cdn.tailwindcss.com"></script>
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 </head>
 
 <style>
+    body {
+        font-family: 'Inter', sans-serif;
+    }
+
     iframe {
         margin-left: auto;
         margin-right: auto;
