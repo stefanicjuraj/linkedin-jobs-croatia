9849a3bf2385c371b15837bf189bcff597be0b08
stefanicjuraj <jurajstefanic@outlook.com>
2023-12-09 12:13:08 +0100

    style: increase sidebar width

--- api/sidebar.php
+++ api/sidebar.php
@@ -37,7 +37,7 @@ $pages = [
     </div>
 </nav>
 
-<aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-24 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0" aria-label="Sidebar">
+<aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-72 h-screen pt-24 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0" aria-label="Sidebar">
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white">
         <ul class="space-y-2 font-medium">
             <?php foreach ($pages as $name => $pageDetails) :
