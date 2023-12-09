350570de7b739f7eb6bb78628007d6f88483c0a3
stefanicjuraj <jurajstefanic@outlook.com>
2023-12-09 22:43:54 +0100

    feat: add system engineering page to pages array

--- api/sidebar.php
+++ api/sidebar.php
@@ -6,6 +6,7 @@ $pages = [
     'Data' => ['url' => '/data', 'icon' => 'analytics-outline'],
     'Full Stack' => ['url' => '/full-stack', 'icon' => 'code-working-outline'],
     'DevOps' => ['url' => '/devops', 'icon' => 'cloud-upload-outline'],
+    'System Engineering' => ['url' => '/system-engineering', 'icon' => 'construct-outline'],
     'Mobile' => ['url' => '/mobile', 'icon' => 'phone-portrait-outline'],
     'Quality Assurance' => ['url' => '/quality-assurance', 'icon' => 'checkmark-circle-outline'],
     'Design' => ['url' => '/design', 'icon' => 'color-palette-outline'],
