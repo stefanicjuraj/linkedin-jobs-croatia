1b16bc8dcec211c971d7fec205357c31f95a8b0e
stefanicjuraj <jurajstefanic@outlook.com>
2023-12-10 20:00:49 +0100

    feat: add widget data scraper to index

--- api/index.php
+++ api/index.php
@@ -60,6 +60,14 @@ if ($_SERVER['REQUEST_URI'] === '/frontend') {
     body {
         font-family: 'Inter', sans-serif;
     }
+
+    iframe {
+        margin-left: auto;
+        margin-right: auto;
+        text-align: center;
+        background-color: #fff;
+        max-width: 100%;
+    }
 </style>
 
 <body>
@@ -81,9 +89,53 @@ if ($_SERVER['REQUEST_URI'] === '/frontend') {
             </div>
         </div>
 
+        <script type="text/javascript">
+            rssfeed_url = new Array();
+            rssfeed_url[0] = "https://politepol.com/fd/9jte6NtKnmC2";
+            rssfeed_url[1] = "https://politepol.com/fd/95W1L5GAzV6p";
+            rssfeed_url[2] = "https://politepol.com/fd/VYOWLy0OlYE8";
+            rssfeed_url[3] = "https://politepol.com/fd/hy5fKt0iNR1f";
+            rssfeed_url[4] = "https://politepol.com/fd/XNC7LQkC3uEj";
+            rssfeed_frame_width = "550";
+            rssfeed_frame_height = "1000";
+            rssfeed_scroll = "off";
+            rssfeed_scroll_step = "4";
+            rssfeed_scroll_bar = "on";
+            rssfeed_target = "_blank";
+            rssfeed_font_size = "15";
+            rssfeed_font_face = "Arial, sans-serif";
+            rssfeed_border = "off";
+            rssfeed_css_url = "https://scraper-widget.vercel.app/linkedin-scraper.css";
+            rssfeed_title = "off";
+            rssfeed_title_name = "";
+            rssfeed_title_bgcolor = "#333";
+            rssfeed_title_color = "#fff";
+            rssfeed_title_bgimage = "";
+            rssfeed_footer = "off";
+            rssfeed_footer_name = "";
+            rssfeed_footer_bgcolor = "#fff";
+            rssfeed_footer_color = "#333";
+            rssfeed_footer_bgimage = "";
+            rssfeed_item_title_length = "100";
+            rssfeed_item_title_color = "#666";
+            rssfeed_item_bgcolor = "#fff";
+            rssfeed_item_bgimage = "";
+            rssfeed_item_border_bottom = "on";
+            rssfeed_item_source_icon = "on";
+            rssfeed_item_date = "off";
+            rssfeed_item_description = "on";
+            rssfeed_item_description_length = "250";
+            rssfeed_item_description_color = "#666";
+            rssfeed_item_description_link_color = "#333";
+            rssfeed_item_description_tag = "on";
+            rssfeed_no_items = "0";
+            rssfeed_cache = "05dac2f49a9b48a20f22bb4ed417b2a3";
+        </script>
+
     </div>
 
     <!-- script -->
+    <script type="text/javascript" src="https://scraper-widget.vercel.app/app.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.js"></script>
 
 </body>
