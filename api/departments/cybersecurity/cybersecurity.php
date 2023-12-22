d30a740a23348106c24df02a1da18ec8c27aa2bb
stefanicjuraj <jurajstefanic@outlook.com>
2023-12-22 01:30:24 +0100

    feat: add cybersecurity

---
+++ api/departments/cybersecurity/cybersecurity.php
@@ -0,0 +1,86 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>linkedin-jobs-croatia</title>
+    <!-- css -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+</head>
+
+<style>
+    body {
+        font-family: 'Inter', sans-serif;
+    }
+
+    iframe {
+        margin-left: auto;
+        margin-right: auto;
+        text-align: center;
+        background-color: #fff;
+        max-width: 100%;
+    }
+</style>
+
+<body>
+
+    <?php require_once __DIR__ . '/../../sidebar.php' ?>
+
+    <div class="mt-10 p-4">
+
+        <script type="text/javascript">
+            rssfeed_url = new Array();
+            rssfeed_url[0] = "https://politepol.com/fd/PQ5skaWn3kzo";
+            rssfeed_frame_width = "550";
+            rssfeed_frame_height = "1000";
+            rssfeed_scroll = "off";
+            rssfeed_scroll_step = "4";
+            rssfeed_scroll_bar = "on";
+            rssfeed_target = "_blank";
+            rssfeed_font_size = "15";
+            rssfeed_font_face = "Arial, sans-serif";
+            rssfeed_border = "off";
+            rssfeed_css_url = "https://scraper-widget.vercel.app/linkedin-scraper.css";
+            rssfeed_title = "off";
+            rssfeed_title_name = "";
+            rssfeed_title_bgcolor = "#333";
+            rssfeed_title_color = "#fff";
+            rssfeed_title_bgimage = "";
+            rssfeed_footer = "off";
+            rssfeed_footer_name = "";
+            rssfeed_footer_bgcolor = "#fff";
+            rssfeed_footer_color = "#333";
+            rssfeed_footer_bgimage = "";
+            rssfeed_item_title_length = "100";
+            rssfeed_item_title_color = "#666";
+            rssfeed_item_bgcolor = "#fff";
+            rssfeed_item_bgimage = "";
+            rssfeed_item_border_bottom = "on";
+            rssfeed_item_source_icon = "on";
+            rssfeed_item_date = "off";
+            rssfeed_item_description = "on";
+            rssfeed_item_description_length = "250";
+            rssfeed_item_description_color = "#666";
+            rssfeed_item_description_link_color = "#333";
+            rssfeed_item_description_tag = "on";
+            rssfeed_no_items = "0";
+            rssfeed_cache = "00cb3f66af3b044a65117b24d92a3e5b";
+        </script>
+
+    </div>
+
+    <!-- script -->
+    <script type="text/javascript" src="https://scraper-widget.vercel.app/app.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.js"></script>
+
+</body>
+
+</html>
